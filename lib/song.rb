611fa6f306083cb91cc46a4feb26540d3ec64f76
sarah <s.j.condon01@gmail.com>
2019-10-11 01:36:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,25 @@
 class Song
   attr_accessor :name, :artist
 
+@@all = []
   def initialize(name)
     @name = name
-    
+    @@all << self
   end
 
-  def self.new_by_filename(file)
-    artist_name = file.split(" - ")[0]
-    song_name = file.split(" - ")[1]
-    song = Song.new(song_name)
-    song.artist_name = artist_name
-    song
+  def self.all
+    @@all
+  end
+  
+  def self.new_by_filename(filename)
+    artist_obj, song = file.split(" - ") #two different variables. "split" >splitting into strings
+    new_song = self.new(song)
+    new_song.artist_name = artist_obj
+    new_song
   end
 
   def artist_name=(name)
     artist = Artist.find_or_create_by_name(name)
-    self.artist = artist
-    artist.add_song(self)
+    # artist.add_song(self)
   end
 end
